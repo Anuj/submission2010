31f3a958e98101f6054aaea300b678cc4ae9cc58
honicky <honicky@d8c37ecb-fc71-b5cd-d94d-9e930947d5ae>
2010-09-24 00:32:37 +0000

    don't do bibtex till we have a bib for speed

--- makepaper.sh
+++ makepaper.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
-xelatex chi_backside_2010
-bibtex chi_backside_2010
-xelatex chi_backside_2010
-xelatex chi_backside_2010
+#leave these out until we have bib entries for faster compiles
+#xelatex chi_backside_2010
+#bibtex chi_backside_2010
+pdflatex chi_backside_2010
+pdflatex chi_backside_2010
